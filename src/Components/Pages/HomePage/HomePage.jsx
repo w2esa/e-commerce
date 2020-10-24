962ca8f28921409e5f60da1c0528f1511e3d1342
w2esa <w2esa2020@gmail.com>
2020-10-24 19:32:37 +0600

    broken our homepage into smaller components, finished styling our menu-item components

---
+++ src/Components/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Directory from "../../Directory/Directory";
+import './homePage.styles.scss'
+
+const HomePage = () => (
+  <section className="homepage">
+   <Directory/>
+  </section>
+);
+export default HomePage;
